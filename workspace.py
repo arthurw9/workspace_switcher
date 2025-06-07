694147a3c5f59d30f284f7334cf3fddc7bb43e2d
Arthur Weinberger <arthurweinberger@gmail.com>
2025-06-07 12:16:12 -0700

    Add commands for list, listwin, and rename.

--- workspace.py
+++ workspace.py
@@ -3,6 +3,25 @@
 import sys
 import subprocess
 
+def help():
+  print("""
+examples:
+  -------------------------------------------------------------------------
+  | workspace help       | Display this help.                             |
+  | workspace move 7 8   | Moves all windows from desktop 7 to desktop 8. |
+  | workspace list       | List all workspaces                            |
+  | workspace listwin    | List all windows                               |
+  | workspace listwin 8  | List all windows in workspace 8                |
+  | workspace rename 3 \ | Rename workspace 3 to "foo bar"                |
+  |     "foo bar"        |                                                |
+  -------------------------------------------------------------------------
+""")
+
+def argv_or(n, default):
+  if len(sys.argv) > n:
+    return sys.argv[n]
+  return default
+
 def run_command(command):
   result = subprocess.run(command, capture_output=True, shell=True, text=True)
   if result.returncode == 0:
@@ -10,7 +29,7 @@ def run_command(command):
   else:
     return f"Error: code {result.returncode}\n{result.stderr}"
 
-def list_windows(desktop):
+def iterate_windows(desktop):
   result = run_command("wmctrl -l")
   windows = result.split("\n")
   for window in windows:
@@ -27,25 +46,50 @@ def list_windows(desktop):
 def move_window_to_desktop(win_id, desktop):
   run_command(f"wmctrl -i -r {win_id} -t {desktop}")
 
-def help():
-  print("""
-examples:
-  workspace move 7 8
-    moves all windows from desktop 7 to desktop 8.
-""")
-
 def move(source_desktop, dest_desktop):
-  for win_id in list_windows(source_desktop):
+  for win_id in iterate_windows(source_desktop):
     move_window_to_desktop(win_id, dest_desktop)
+
+def list_workspaces():
+  print(run_command("wmctrl -d"))
+
+def list_windows(desktop):
+  result = run_command("wmctrl -l")
+  if desktop == "none":
+    print(result)
+    return
+  windows = result.split("\n")
+  for window in windows:
+    a = window.split(" ")
+    if len(a) < 3:
+      continue
+    win_id = a[0]
+    i = 1
+    while a[i].strip() == "":
+      i += 1
+    if desktop == a[i]:
+      print(window)
+
+def rename(desktop, new_name):
+  print(run_command(f"gsettings set org.mate.Marco.workspace-names name-{desktop} \"{new_name}\""))
 
 def main():
   command = sys.argv[1]
   if command == "help":
     help()
   if command == "move":
-    source_desktop = sys.argv[2]
-    dest_desktop = sys.argv[3]
+    source_desktop = argv_or(2, "none")
+    dest_desktop = argv_or(3, "none")
     move(source_desktop, dest_desktop)
+  if command == "list":
+    list_workspaces()
+  if command == "listwin":
+    desktop = argv_or(2, "none")
+    list_windows(desktop)
+  if command == "rename":
+    desktop = argv_or(2, "none")
+    new_name = argv_or(3, "none")
+    rename(desktop, new_name)
 
 if __name__ == "__main__":
   main()
