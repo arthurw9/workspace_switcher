dfba9de52e434ab404e515edf421847e6fa80c2b
Arthur Weinberger <arthurweinberger@gmail.com>
2025-06-19 15:25:02 -0700

    Renaming a workspace handles spaces better.

--- rename_workspace.sh
+++ rename_workspace.sh
@@ -8,9 +8,8 @@ else
   desktop_number=$(wmctrl -d | grep '*' | awk '{ print $1 }')
   desktop_number=$(expr $desktop_number + 1)
   name=$(gsettings get org.mate.Marco.workspace-names name-$desktop_number)
-  name=$(echo $name | tr -d "'")
-  new_name=$(zenity --text="Rename Current Workspace" --entry --entry-text=$name)
+  new_name=$(zenity --text="Rename Current Workspace" --entry --entry-text="$name")
   if ! [ -z "$new_name" ]; then
-    gsettings set org.mate.Marco.workspace-names name-$desktop_number $new_name
+    gsettings set org.mate.Marco.workspace-names name-$desktop_number "$new_name"
   fi
 fi
